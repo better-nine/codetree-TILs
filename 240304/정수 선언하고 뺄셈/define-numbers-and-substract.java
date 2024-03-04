281b05a0aaf74b30510edd6a6dd240de9e976482
better-nine <76273087+better-nine@users.noreply.github.com>
2024-03-04 15:12:31 +0900

    Title: 정수 선언하고 뺄셈, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

--- 240304/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 240304/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -3,7 +3,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a = 97;
         int b = 13;
-        System.out.print(a - b);
+        int c = a - b;
+        System.out.print(a + " - " + b + " = " + c);
 
     }
 }
