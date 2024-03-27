ebcf4a41a91fd875e1e224df7da9a9f75698389d
better-nine <76273087+better-nine@users.noreply.github.com>
2024-03-27 10:19:58 +0900

    Title: 변수 값 교체하기 3, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240327/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3;
+        int b = 5;
+        int temp = b;
+        b = a;
+        a = temp;
+        
+                System.out.println(a);
+                System.out.println(b);
+    }
+}
