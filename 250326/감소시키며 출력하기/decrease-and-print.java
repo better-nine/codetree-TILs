20cbfabe17cd169b146275b8ec45e573792d292f
better-nine <76273087+better-nine@users.noreply.github.com>
2025-03-26 10:16:47 +0900

    Title: 감소시키며 출력하기, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250326/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        for (int i = 17; i > 4; i--) {
+            System.out.print(i+" ");
+        }
+
+    }
+}
